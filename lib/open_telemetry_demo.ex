4d811abad758002afced76d364ec948764e119c8
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-09-13 19:32:07 -0700

    initial commit

---
+++ lib/open_telemetry_demo.ex
@@ -0,0 +1,18 @@
+defmodule OpenTelemetryDemo do
+  @moduledoc """
+  Documentation for `OpenTelemetryDemo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpenTelemetryDemo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
